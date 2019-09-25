47e73784b0b116c83a64c3bce195978ff6259137
Rafael Faita <rfaita@gmail.com>
2019-09-25 17:28:19 +0100

    implementation of add and findById

--- src/main/java/com/academy/tests/BookStore.java
+++ src/main/java/com/academy/tests/BookStore.java
@@ -16,4 +16,6 @@ public interface BookStore {
 
     List<Book> findAllByName(String name);
 
+
+
 }
--- src/main/java/com/academy/tests/domain/Book.java
+++ src/main/java/com/academy/tests/domain/Book.java
@@ -16,15 +16,13 @@ public class Book {
         return id;
     }
 
-       public String getName() {
+    public String getName() {
         return name;
     }
-
 
     public String getIsbn() {
         return isbn;
     }
-
 
 
 }
